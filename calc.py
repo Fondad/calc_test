6e948da9bd9504abb5886b484a8762efd90c4fa0
Vsevolod Demidov <fondad@ukr.net>
2022-04-12 13:27:44 +0300

    Fuction calc add two numbers

--- calc.py
+++ calc.py
@@ -1,5 +1,11 @@
 # Simple calculator. Test example
-print ('Result = ...')
 
-def calc():
-    pass
+a = 5
+b = 10
+
+def calc(x,y):
+    return x + y
+
+z = calc(a,b)
+
+print ('Result = {}'.format(z))
