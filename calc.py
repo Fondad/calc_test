e2898f68c547a002748694af22946cdcfbd328e7
Vsevolod Demidov <fondad@ukr.net>
2022-04-18 11:31:28 +0300

    fixed the broken email address

--- calc.py
+++ calc.py
@@ -1,8 +1,8 @@
 # Simple calculator. Test example
 # Author: @Vsevolod Demidov (fondad@gmail.com)
 
-a = 5
-b = 10
+a = 14
+b = 33
 
 def calc(x,y):
     # Add two numbers
@@ -10,4 +10,5 @@ def calc(x,y):
 
 z = calc(a,b)
 
+print('++++++++++++++++++++')
 print ('Result = {}'.format(z))
